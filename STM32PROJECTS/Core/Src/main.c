6fce6e151bf1a2e6395e6bc50ef2c1c5072bf5f2
yshic <tuanl799@gmail.com>
2023-10-22 14:56:58 +0700

    config timer

--- STM32PROJECTS/Core/Src/main.c
+++ STM32PROJECTS/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "software_timer.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -89,7 +89,7 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_TIM_Base_Start_IT(&htim2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
