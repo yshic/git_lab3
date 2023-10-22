cb3087d9ab8848b8b5b695ed579a140e8b7fc250
yshic <tuanl799@gmail.com>
2023-10-22 14:52:23 +0700

    config pinout

--- STM32PROJECTS/Core/Inc/main.h
+++ STM32PROJECTS/Core/Inc/main.h
@@ -57,6 +57,42 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_RED0_Pin GPIO_PIN_3
+#define LED_RED0_GPIO_Port GPIOA
+#define LED_AMBER0_Pin GPIO_PIN_4
+#define LED_AMBER0_GPIO_Port GPIOA
+#define LED_GREEN0_Pin GPIO_PIN_5
+#define LED_GREEN0_GPIO_Port GPIOA
+#define LED_RED1_Pin GPIO_PIN_6
+#define LED_RED1_GPIO_Port GPIOA
+#define LED_AMBER1_Pin GPIO_PIN_7
+#define LED_AMBER1_GPIO_Port GPIOA
+#define SEG_A_Pin GPIO_PIN_0
+#define SEG_A_GPIO_Port GPIOB
+#define SEG_B_Pin GPIO_PIN_1
+#define SEG_B_GPIO_Port GPIOB
+#define SEG_C_Pin GPIO_PIN_2
+#define SEG_C_GPIO_Port GPIOB
+#define LED_GREEN1_Pin GPIO_PIN_8
+#define LED_GREEN1_GPIO_Port GPIOA
+#define BT1_Pin GPIO_PIN_9
+#define BT1_GPIO_Port GPIOA
+#define BT2_Pin GPIO_PIN_10
+#define BT2_GPIO_Port GPIOA
+#define BT3_Pin GPIO_PIN_11
+#define BT3_GPIO_Port GPIOA
+#define EN0_Pin GPIO_PIN_12
+#define EN0_GPIO_Port GPIOA
+#define EN1_Pin GPIO_PIN_13
+#define EN1_GPIO_Port GPIOA
+#define SEG_D_Pin GPIO_PIN_3
+#define SEG_D_GPIO_Port GPIOB
+#define SEG_E_Pin GPIO_PIN_4
+#define SEG_E_GPIO_Port GPIOB
+#define SEG_F_Pin GPIO_PIN_5
+#define SEG_F_GPIO_Port GPIOB
+#define SEG_G_Pin GPIO_PIN_6
+#define SEG_G_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
