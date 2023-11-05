56be3ba238c836052654f27b87b0b35849709c98
yshic <tuanl799@gmail.com>
2023-11-05 18:01:42 +0700

    add TICK

--- STM32PROJECTS/Core/Src/software_timer.c
+++ STM32PROJECTS/Core/Src/software_timer.c
@@ -1,10 +1,11 @@
 #include "software_timer.h"
+#define TICK 10
 
 int timer_counter[16] = {0};
 int timer_flag[16] = {0};
 
 void setTimer(int duration, int id){
-	timer_counter[id] = duration;
+	timer_counter[id] = duration/TICK;
 	timer_flag[id] = 0;
 }
 void timerRun(int id){
