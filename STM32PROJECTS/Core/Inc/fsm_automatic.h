2680a8f73b60b33d305af49daafe1abc60db6bb4
yshic <tuanl799@gmail.com>
2023-11-05 14:07:39 +0700

    init fsm_automatic

---
+++ STM32PROJECTS/Core/Inc/fsm_automatic.h
@@ -0,0 +1,13 @@
+/*
+ * fsm_automatic.h
+ *
+ *  Created on: Nov 5, 2023
+ *      Author: Papa
+ */
+
+#ifndef INC_FSM_AUTOMATIC_H_
+#define INC_FSM_AUTOMATIC_H_
+
+
+
+#endif /* INC_FSM_AUTOMATIC_H_ */
---
+++ STM32PROJECTS/Core/Src/fsm_automatic.c
@@ -0,0 +1,8 @@
+/*
+ * fsm_automatic.c
+ *
+ *  Created on: Nov 5, 2023
+ *      Author: Papa
+ */
+
+
